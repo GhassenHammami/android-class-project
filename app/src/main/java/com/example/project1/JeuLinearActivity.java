1b976afaaf52c9bc7501b9bd2a0944d1433ec738
GhassenHammami <mohamedghassen.hammami@polytechnicien.tn>
2025-10-14 18:48:20 +0300

    Finalize Atelier 2

--- app/src/main/java/com/example/project1/JeuLinearActivity.java
+++ app/src/main/java/com/example/project1/JeuLinearActivity.java
@@ -1,5 +1,6 @@
 package com.example.project1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,8 +22,10 @@ public class JeuLinearActivity extends AppCompatActivity {
     private Button btnValidate;
     private EditText editNumberInput;
     private TextView textAttemptNumber;
+    private TextView textMessage;
 
     private int attemptCount = 1;
+    private int randomNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,50 +36,73 @@ public class JeuLinearActivity extends AppCompatActivity {
         btnValidate = findViewById(R.id.btnVerifyUser);
         editNumberInput = findViewById(R.id.editNombreUser);
         textAttemptNumber = findViewById(R.id.txtAttempt);
+        textMessage = findViewById(R.id.txtMessage);
 
-        String attemptText = getString(R.string.attempt_text, attemptCount);
-        textAttemptNumber.setText(attemptText);
+        initializeGame();
 
-        final int min = 100;
-        final int max = 999;
-        final int randomNumber = new Random().nextInt((max - min) + 1) + min;
-        Log.d("Jeu", "Random number generated: " + randomNumber);
-
-        btnValidate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnValidate.setOnClickListener(v -> {
+            if (attemptCount >= 10)
+                initializeGame();
+            else {
                 String nbr = editNumberInput.getText().toString().trim();
-
                 if (nbr.isEmpty()) {
-                    Log.e("Jeu", "Vous devez saisir un nombre");
+                    displayMessage("Vous devez saisir un nombre", Color.RED);
                 } else {
                     try {
                         int nbr_saisi = Integer.parseInt(nbr);
-                        if (nbr_saisi < 100 || nbr_saisi > 999) {
-                            Log.e("Jeu", "Veuillez saisir un nombre entre 100 et 999");
-                        } else {
-                            if (attemptCount >= 10) {
-                                Log.d("Jeu", "10 tentatives effectués ");
+                        if (nbr_saisi < 100 || nbr_saisi > 999)
+                            displayMessage("Veuillez saisir un nombre entre 100 et 999", Color.RED);
+                        else if (attemptCount >= 10)
+                            displayMessage("10 tentatives déja effectués", Color.RED);
+                        else {
+                            Log.d("Jeu", "Nombre saisi est = " + nbr_saisi);
+
+                            if (nbr_saisi == randomNumber) {
+                                displayMessage("Nombre correcte", 0xff99cc00);
+                            } else if (nbr_saisi < randomNumber) {
+                                displayMessage("Nombre incorrecte. Essayez un nombre supérieur.", Color.YELLOW);
                             } else {
-                                Log.d("Jeu", "Nombre saisi est = " + nbr_saisi);
-
-                                if (nbr_saisi == randomNumber)
-                                    Log.e("Jeu", "Nombre correcte" );
-                                else
-                                    Log.e("Jeu", "Nombre incorrecte" );
-
-                                final int newAttemptCount = attemptCount + 1;
-                                String attemptText = getString(R.string.attempt_text, newAttemptCount);
-                                textAttemptNumber.setText(attemptText);
-                                attemptCount = newAttemptCount;
+                                displayMessage("Nombre incorrecte. Essayez un nombre inférieur.", Color.YELLOW);
                             }
 
+                            final int newAttemptCount = attemptCount + 1;
+                            String attemptText = getString(R.string.attempt_text, newAttemptCount);
+                            textAttemptNumber.setText(attemptText);
+                            attemptCount = newAttemptCount;
                         }
+
+                        if (attemptCount >= 10) {
+                            displayMessage("Vous avez perdu ! Le bon nombre était : " + randomNumber, Color.RED);
+                            btnValidate.setText("Reset");
+                        }
+
                     } catch (NumberFormatException e) {
-                        Log.e("Jeu", "Entrée invalide : " + nbr);
+                        displayMessage("Entrée invalide : " + nbr, Color.RED);
                     }
                 }
             }
         });
+    }
+
+    private void initializeGame() {
+        attemptCount = 1;
+        String attemptText = getString(R.string.attempt_text, attemptCount);
+        textAttemptNumber.setText(attemptText);
+
+        editNumberInput.setText("");
+        textMessage.setVisibility(View.INVISIBLE);
+        btnValidate.setText("Vérifier");
+
+        final int min = 100;
+        final int max = 999;
+        randomNumber = new Random().nextInt((max - min) + 1) + min;
+        Log.d("Jeu", "Random number generated: " + randomNumber);
+    }
+
+    public void displayMessage(String message, int color) {
+        textMessage.setVisibility(View.VISIBLE);
+        textMessage.setText(message);
+        textMessage.setTextColor(color);
+        Log.e("Jeu", message);
     }
 }
