4d237631ffaaba7387910985acc1df480bd9e37c
GhassenHammami <mohamedghassen.hammami@polytechnicien.tn>
2025-10-21 21:33:52 +0100

    WIP Atelier 5

--- app/src/main/java/com/example/project1/CourseSelectionActivity.java
+++ app/src/main/java/com/example/project1/CourseSelectionActivity.java
@@ -1,6 +1,8 @@
 package com.example.project1;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CourseSelectionActivity extends AppCompatActivity {
 
+    private EditText nameEdit;
+    private EditText emailEdit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_course_selection);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        nameEdit = findViewById(R.id.courseSelectionNameEdit);
+        emailEdit = findViewById(R.id.courseSelectPhoneEdit);
+
+        String name = nameEdit.getText().toString().trim();
+        String email = emailEdit.getText().toString().trim();
+
+        if (name.isEmpty() || email.isEmpty()) {
+            Toast.makeText(this, "Veuiller remplir le nom et l'email.", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
