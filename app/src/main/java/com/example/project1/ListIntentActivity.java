7b5f991fe83f59b2e75333df552aac113efb9632
GhassenHammami <mohamedghassen.hammami@polytechnicien.tn>
2025-11-21 18:14:04 +0100

    Rename SimpleExplicitIntentReturnActivity > SimpleExplicitIntentActivity

--- app/src/main/java/com/example/project1/ListIntentActivity.java
+++ app/src/main/java/com/example/project1/ListIntentActivity.java
@@ -2,7 +2,6 @@ package com.example.project1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,7 +21,7 @@ public class ListIntentActivity extends AppCompatActivity {
         closeBtn = findViewById(R.id.listIntentBtnClose);
 
         explicitIntentBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(ListIntentActivity.this, SimpleExplicitIntentReturnActivity.class);
+            Intent intent = new Intent(ListIntentActivity.this, SimpleExplicitIntentActivity.class);
             startActivity(intent);
         });
 
--- app/src/main/java/com/example/project1/SimpleExplicitIntentActivity.java
+++ app/src/main/java/com/example/project1/SimpleExplicitIntentActivity.java
@@ -3,27 +3,23 @@ package com.example.project1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class SimpleExplicitIntentReturnActivity extends AppCompatActivity {
+public class SimpleExplicitIntentActivity extends AppCompatActivity {
 
     Button closeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_simple_explicit_intent_return);
+        setContentView(R.layout.activity_simple_explicit_intent);
 
         closeBtn = findViewById(R.id.simpleExplicitIntentReturnBtn);
 
         closeBtn.setOnClickListener(v -> {
-            Intent intent = new Intent(SimpleExplicitIntentReturnActivity.this, ListIntentActivity.class);
+            Intent intent = new Intent(SimpleExplicitIntentActivity.this, ListIntentActivity.class);
             startActivity(intent);
         });
 
